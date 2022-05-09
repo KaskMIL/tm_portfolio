5c37b8fa7a2eb5090fb3496ec2e66c119d64a1b3
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-05-09 17:06:31 -0300

    Added eventlistener to mobile-menu
    - X button close mobile-menu
    - Portfolio, About and Conctact session buttons, display the correct section of the project

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-$logoId = document.querySelector("#logo-id");
-$mobileHamburgerButton = document.querySelector("#mobile-menu-btn");
-$mobileNav = document.querySelector("#mobile-nav");
-$closeButton = document.querySelector("#close-button");
+$logoId = document.querySelector('#logo-id');
+$mobileHamburgerButton = document.querySelector('#mobile-menu-btn');
+$mobileNav = document.querySelector('#mobile-nav');
+$closeButton = document.querySelector('#close-button');
 
-$portfolioNavMobile = document.querySelector("#portfolio-nav-mobile");
-$aboutNavMobile = document.querySelector("#about-nav-mobile");
-$contactNavMobile = document.querySelector("#contact-nav-mobile");
+$portfolioNavMobile = document.querySelector('#portfolio-nav-mobile');
+$aboutNavMobile = document.querySelector('#about-nav-mobile');
+$contactNavMobile = document.querySelector('#contact-nav-mobile');
 
-$mobileHamburgerButton.addEventListener("click", e => {
-    $logoId.classList.add("mobile-menu-off");
-    $mobileHamburgerButton.classList.add("mobile-menu-off");
-    $mobileNav.classList.remove("mobile-menu-off");
-    $mobileNav.classList.add("mobile-menu-on");
+$mobileHamburgerButton.addEventListener('click', (e) => {
+	$logoId.classList.add('mobile-menu-off');
+	$mobileHamburgerButton.classList.add('mobile-menu-off');
+	$mobileNav.classList.remove('mobile-menu-off');
+	$mobileNav.classList.add('mobile-menu-on');
+});
+
+function hideMobileMenu() {
+	$mobileNav.classList.remove('mobile-menu-on');
+	$mobileNav.classList.add('mobile-menu-off');
+	$logoId.classList.remove('mobile-menu-off');
+	$mobileHamburgerButton.classList.remove('mobile-menu-off');
+}
+
+$closeButton.addEventListener('click', (e) => {
+	hideMobileMenu();
+});
+
+$portfolioNavMobile.addEventListener('click', (e) => {
+	hideMobileMenu();
+});
+
+$aboutNavMobile.addEventListener('click', (e) => {
+	hideMobileMenu();
+});
+
+$contactNavMobile.addEventListener('click', (e) => {
+	hideMobileMenu();
 });
