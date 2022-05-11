4cc0559964721a74713ec47a5583b254464f0a08
toMilanesi <tomi.milanesi@gmail.com>
2022-05-11 12:02:45 -0300

    Fix spacing errors

--- script.js
+++ script.js
@@ -1,119 +1,118 @@
 // DOM Variables
-const $logoId = document.getElementById('logo-id');
-const $mobileHamburgerButton = document.getElementById('mobile-menu-btn');
-const $mobileNav = document.getElementById('mobile-nav');
-const $closeButton = document.getElementById('close-button');
+const $logoId = document.getElementById("logo-id");
+const $mobileHamburgerButton = document.getElementById("mobile-menu-btn");
+const $mobileNav = document.getElementById("mobile-nav");
+const $closeButton = document.getElementById("close-button");
 
-const $portfolioNavMobile = document.getElementById('portfolio-nav-mobile');
-const $aboutNavMobile = document.getElementById('about-nav-mobile');
-const $contactNavMobile = document.getElementById('contact-nav-mobile');
+const $portfolioNavMobile = document.getElementById("portfolio-nav-mobile");
+const $aboutNavMobile = document.getElementById("about-nav-mobile");
+const $contactNavMobile = document.getElementById("contact-nav-mobile");
 
 // Functions
 function hideMobileMenu() {
-  $mobileNav.classList.remove('mobile-menu-on');
-  $mobileNav.classList.add('mobile-menu-off');
-  $logoId.classList.remove('mobile-menu-off');
-  $mobileHamburgerButton.classList.remove('mobile-menu-off');
+  $mobileNav.classList.remove("mobile-menu-on");
+  $mobileNav.classList.add("mobile-menu-off");
+  $logoId.classList.remove("mobile-menu-off");
+  $mobileHamburgerButton.classList.remove("mobile-menu-off");
 }
 
 // DOM Manipulation
-$mobileHamburgerButton.addEventListener('click', () => {
-  $logoId.classList.add('mobile-menu-off');
-  $mobileHamburgerButton.classList.add('mobile-menu-off');
-  $mobileNav.classList.remove('mobile-menu-off');
-  $mobileNav.classList.add('mobile-menu-on');
+$mobileHamburgerButton.addEventListener("click", () => {
+  $logoId.classList.add("mobile-menu-off");
+  $mobileHamburgerButton.classList.add("mobile-menu-off");
+  $mobileNav.classList.remove("mobile-menu-off");
+  $mobileNav.classList.add("mobile-menu-on");
 });
 
-$closeButton.addEventListener('click', () => {
+$closeButton.addEventListener("click", () => {
   hideMobileMenu();
 });
 
-$portfolioNavMobile.addEventListener('click', () => {
+$portfolioNavMobile.addEventListener("click", () => {
   hideMobileMenu();
 });
 
-$aboutNavMobile.addEventListener('click', () => {
+$aboutNavMobile.addEventListener("click", () => {
   hideMobileMenu();
 });
 
-$contactNavMobile.addEventListener('click', () => {
+$contactNavMobile.addEventListener("click", () => {
   hideMobileMenu();
 });
 
-
-const $worksSection = document.getElementById('works-section');
+const $worksSection = document.getElementById("works-section");
 
 const ProjectList = [{
-    title: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text',
-    featureImg: 'images/ImgProj.png',
+    title: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text",
+    featureImg: "images/ImgProj.png",
     tecnologies: {
-      firstTec: 'css',
-      secondTec: 'html',
-      thirdTec: 'Bootstrap',
-      fourthTec: 'Ruby',
+      firstTec: "css",
+      secondTec: "html",
+      thirdTec: "Bootstrap",
+      fourthTec: "Ruby",
     },
   },
   {
-    title: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    title: "Profesional Art Printing Data",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard",
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
   },
   {
-    title: 'Data Dashboard Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    title: "Data Dashboard Healthcare",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard",
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
   },
   {
-    title: 'Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    title: "Website Portfolio",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard",
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
   },
   {
-    title: 'Data Daschboard Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    title: "Data Daschboard Healthcare",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard",
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
   },
   {
     title: 'Data Daschboard Healthcare"',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard",
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
   },
   {
     title: 'Data Daschboard Healthcare", description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standar',
     tecnologies: {
-      firstTec: 'html',
-      secondTec: 'Bootstrap',
-      thirdTec: 'Ruby',
+      firstTec: "html",
+      secondTec: "Bootstrap",
+      thirdTec: "Ruby",
     },
-  }
+  },
 ];
 
 for (let i = 0; i < ProjectList.length; i += 1) {
-  const card = document.createElement('article');
+  const card = document.createElement("article");
   const count = i;
   if (i === 0) {
-    card.classList.add('card', 'card-desktop');
+    card.classList.add("card", "card-desktop");
     card.innerHTML = `<img src="${ProjectList[i].featureImg}" alt="Main Project image">
                 <section class="card-content">
                     <h3>${ProjectList[i].title}</h3>
@@ -128,7 +127,7 @@ for (let i = 0; i < ProjectList.length; i += 1) {
                 </section>`;
     $worksSection.appendChild(card);
   } else {
-    card.classList.add('card', 'card-bg', `card-bg-${count}`);
+    card.classList.add("card", "card-bg", `card-bg-${count}`);
     card.innerHTML = `
                 <h3>${ProjectList[i].title}</h3>
                 <p>${ProjectList[i].description}</p>
